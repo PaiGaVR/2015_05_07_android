a9e084572ed39d20cde1e0b84830212efc314bfd
GeXiaoDong <421536265@qq.com>
2015-05-07 16:59:03 +0800

    页面初步搭建

--- src/com/example/com/android/test/MainActivity.java
+++ src/com/example/com/android/test/MainActivity.java
@@ -1,23 +1,76 @@
 package com.example.com.android.test;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TabHost;
 
-public class MainActivity extends Activity {
-
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
+	private TabHost tab_host;
+	private RadioGroup main_radio_group;
+	private RadioButton push_orders_button, get_orders_button, read_orders_button, setting_button;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        // 获取按钮
+        main_radio_group = (RadioGroup)findViewById(R.id.main_radiogroup);
+        
+        push_orders_button = (RadioButton)findViewById(R.id.tab_icon_push_orders);
+        get_orders_button = (RadioButton)findViewById(R.id.tab_icon_get_orders);
+        read_orders_button = (RadioButton)findViewById(R.id.tab_icon_read_orders);
+        setting_button = (RadioButton)findViewById(R.id.tab_icon_setting);
+        
+        // 往TabWidget添加Tab
+        tab_host = getTabHost();
+        tab_host.addTab(tab_host.newTabSpec("tag1").setIndicator("0").setContent(new Intent(this, PushOrdersActivity.class)));
+        tab_host.addTab(tab_host.newTabSpec("tag2").setIndicator("1").setContent(new Intent(this, GetOrdersActivity.class)));
+        tab_host.addTab(tab_host.newTabSpec("tag3").setIndicator("2").setContent(new Intent(this, ReadOrdersActivity.class)));
+        tab_host.addTab(tab_host.newTabSpec("tag4").setIndicator("3").setContent(new Intent(this, SettingActivity.class)));
+        
+        CheckListener checkListener = new CheckListener();
+        main_radio_group.setOnCheckedChangeListener(checkListener);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public class CheckListener implements OnCheckedChangeListener{
+    	
+		@Override
+		public void onCheckedChanged(RadioGroup arg0, int arg1) {
+			//setCurrentTab 通过标签索引设置当前显示的内容
+	        //setCurrentTabByTag 通过标签名设置当前显示的内容
+			switch(arg1){
+				case R.id.tab_icon_push_orders : {
+					tab_host.setCurrentTab(0);
+					break;
+				}
+				case R.id.tab_icon_get_orders : {
+					tab_host.setCurrentTab(1);
+					break;
+				}
+				case R.id.tab_icon_read_orders : {
+					tab_host.setCurrentTab(2);
+					break;
+				}
+				case R.id.tab_icon_setting : {
+					tab_host.setCurrentTab(3);
+					break;
+				}
+			}
+		}
     }
     
 }
