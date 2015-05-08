e654bdc1a40d2cca001b3d73d699c08aee22150a
GeXiaoDong <421536265@qq.com>
2015-05-08 14:38:09 +0800

    GXD

--- src/com/example/com/android/test/MyRadioGroup.java
+++ src/com/example/com/android/test/MyRadioGroup.java
@@ -2,15 +2,19 @@ package com.example.com.android.test;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.RadioGroup;
+import android.widget.TabHost;
 
 public class MyRadioGroup extends RadioGroup {
 	private CheckListener checkListener;
+	private TabHost tabHost;
+	private View view;
 
 	public MyRadioGroup(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		checkListener = new CheckListener();
-		this.setOnCheckedChangeListener(checkListener);
+		/*checkListener = new CheckListener();
+		this.setOnCheckedChangeListener(checkListener);*/
 	}
 	
 public class CheckListener implements OnCheckedChangeListener{
